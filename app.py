c853cb5893f4ff96a25e0ba8ed956508120f75cb
madhavipuliraju <madhavipuliraju@gmail.com>
2019-11-17 11:02:02 +0530

    implemented decode and protected api

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 from flask import Flask, request, make_response, jsonify
 import jwt
+import datetime
 from functools import wraps
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "this is the secret key"
 		
-def token_required(func):
-	@wraps(func)
-	def decorated(*args, **kwagrs):
+def token_required(f):
+	@wraps(f)
+	def decorated(*args, **kwargs):
 		token = request.args.get('token')
 		print token
 		if not token:
 			return jsonify({'message': 'Token is missing'}), 403
 		try:
-			data = jwt.decode(JSON.parse(token), app.config['SECRET_KEY'])
+			data = jwt.decode(token, app.config['SECRET_KEY'])
 		except:
 			return jsonify({'message': 'Token is invalid'}), 403
+		return f(*args, **kwargs)
 	return decorated
 
 @app.route('/protected')
@@ -28,7 +30,7 @@ def protected():
 def login():
 	auth = request.authorization
 	if auth:
-		token = jwt.encode({'user':auth.username},'secret', algorithm='HS256')
+		token = jwt.encode({'user':auth.username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
 		return jsonify({'token':token})
 
 	return make_response("Un Authorized", 401, {'WWW-Authenticate':'Basic-realm="Login required!"'})
