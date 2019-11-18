1a89252c9a75678330f4c9589cea8a0275982557
madhavipuliraju <madhavipuliraju@gmail.com>
2019-11-18 15:00:20 +0530

    upgrade virtual environment

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -57,7 +57,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/vlead/projects/python3-assignment/venv"
+set -gx VIRTUAL_ENV "/home/vlead/projects/python-assignment/venv"
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | tr "." "\n")[1] -lt 3
