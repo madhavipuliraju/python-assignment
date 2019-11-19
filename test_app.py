e872f393a842ca4941eb327ff200b4ce2d9325d9
madhavipuliraju <madhavipuliraju@gmail.com>
2019-11-19 10:25:16 +0530

    completed test cases

--- test_app.py
+++ test_app.py
@@ -5,24 +5,53 @@ import requests, json, base64
 from requests.auth import HTTPBasicAuth
 
 class TestLogin(TestCase):
-    TESTING = True
 
     def create_app(self):
         app = create_app()
         return app
 
+    def setUp(self):  
+        self.url = 'http://localhost:5000/login'
+
     def runTest(self):  
+        valid_credentials = base64.b64encode(b'testuser:testpassword').decode('utf-8')
+        response = self.client.get(self.url, headers={'Authorization': 'Basic ' + valid_credentials})
+        self.assertEqual(response.status_code, 200)
+
+class TestLoginWithoutAuth(TestCase):
+
+    def create_app(self):
+        app = create_app()
+        return app
+
+    def setUp(self):  
+        self.url = 'http://localhost:5000/login'
+
+    def runTest(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 401)
+
+class TestImage(TestCase):
+
+    def create_app(self):
+        app = create_app()
+        return app
+                  
+    def setUp(self):  
+        self.image_url = 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png'
         url = 'http://localhost:5000/login'
         valid_credentials = base64.b64encode(b'testuser:testpassword').decode('utf-8')
         response = self.client.get(url, headers={'Authorization': 'Basic ' + valid_credentials})
-        self.assertEqual(response.status_code, 200)
+        response_dict = json.loads(response.data)
+        self.token = response_dict['token']
+        return self.token
 
-    def runTestLoginWithoutAuth(self):
-        url = 'http://localhost:5000/login'
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 201)
+    def runTest(self):
+        thumb_url = 'http://localhost:5000/thumbnail?token='+ self.token + '&url=' + self.image_url
+        response1 = requests.get(thumb_url)
+        self.assertEqual(response1.status_code, 200)
 
-class TestImage(TestCase):
+class TestPatch(TestCase):
 
     def create_app(self):
         app = create_app()
@@ -35,41 +64,30 @@ class TestImage(TestCase):
         response_dict = json.loads(response.data)
         self.token = response_dict['token']
         return self.token
-
-    def runTest(self):
-        thumb_url = 'http://localhost:5000/thumbnail?token='+ self.token +'&url=https://homepages.cae.wisc.edu/~ece533/images/airplane.png'
-        response1 = requests.get(thumb_url)
-        self.assertEqual(response1.status_code, 200)
-
-# class TestPatch(TestCase):
                   
-#     def runTest(self):  
-#         url = 'http://localhost:5000/login'
-#         response = requests.get(url, auth=HTTPBasicAuth('user', 'pass'))
-#         if response.ok:
-#            token = json.loads(response.text)['token']
-#            patch_url = 'http://localhost:5000/patch?token='+ token
-#            data = {
-#                "json":{
-#                "baz": "qux",
-#                  "foo": "bar"
-#                  },
-# 	         "patch":[
-#                    { "op": "replace", "path": "/baz", "value": "boo" },
-#                    { "op": "add", "path": "/hello", "value": ["world"] },
-#                    { "op": "remove", "path": "/foo" }
-#                    ]
-#                    }
-#            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-#            response = requests.patch(patch_url, data=json.dumps(data), headers=headers)
-#            str = {
-#              "baz": "boo", 
-#              "hello": [
-#              "world"
-#              ]
-#              }
-#            self.assertEqual(json.loads(response.text), str)
-#            self.assertEqual(response.status_code, 200)
+    def runTest(self):  
+        patch_url = 'http://localhost:5000/patch?token='+ self.token
+        data = {
+               "json":{
+               "baz": "qux",
+                 "foo": "bar"
+                 },
+	         "patch":[
+                   { "op": "replace", "path": "/baz", "value": "boo" },
+                   { "op": "add", "path": "/hello", "value": ["world"] },
+                   { "op": "remove", "path": "/foo" }
+                   ]
+                }
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        response = requests.patch(patch_url, data=json.dumps(data), headers=headers)
+        str = {
+             "baz": "boo", 
+             "hello": [
+             "world"
+           ]
+          }
+        self.assertEqual(json.loads(response.text), str)
+        self.assertEqual(response.status_code, 200)
         
 suite = unittest.TestSuite()
 
