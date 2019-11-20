008fe098d918286f4d7120f1b7bfecf2942e9a95
madhavipuliraju <madhavipuliraju@gmail.com>
2019-11-20 15:02:24 +0530

    add create app outside of main

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def create_app():
     app.logger.setLevel(logging.ERROR)
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True, host='0.0.0.0', threaded=True)
